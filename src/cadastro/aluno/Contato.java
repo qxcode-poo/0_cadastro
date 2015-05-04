57a3c090e1e3f2891a020ac08c729a24cba195e6
ronildo <ronildo@rwx-pc>
2015-05-04 19:11:45 -0300

    Versao Concreta Respostas

--- src/cadastro/aluno/Contato.java
+++ src/cadastro/aluno/Contato.java
@@ -1,28 +1,34 @@
 package cadastro.aluno;
 
 public class Contato {
-
-	public Contato(String nome, String telefone) {
-		
-	}
 	
+	private String nome;
+	private String telefone;
+	
+	//Método construtor da classe
+	public Contato(int id, String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+
 	//Retorna Nome do Usuário
 	public String getNome() {
-		return null;
+		return this.nome;
 	}
 
 	//Atribui valor ao Nome do Usuário
 	public void setNome(String nome) {
-		
+		this.nome = nome;
 	}
-
-	//Retorna Telefone do Usuário
+		
+	//Retorna Telefone do Usuário	
 	public String getTelefone() {
-		return null;
+		return this.telefone;
 	}
 	
 	//Atribui valor ao Telefone do Usuário
 	public void setTelefone(String telefone) {
-
+		this.telefone = telefone;
 	}
+
 }
