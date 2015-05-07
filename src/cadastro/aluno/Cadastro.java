8129e251abedb76f6633cb066e4e1da5156b621c
ronildo <ronildo@rwx-pc>
2015-05-06 23:06:15 -0300

    Gui

--- src/cadastro/aluno/Cadastro.java
+++ src/cadastro/aluno/Cadastro.java
@@ -3,35 +3,28 @@ package cadastro.aluno;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cadastro{
+public class Cadastro {
 	
-	private List<Contato> listaContatos;
+	private List<Contato> listaContatos = new ArrayList<Contato>();
 	private int maxContatos = 0;
 	
-	//Complete a função que verifique se a string é feita apenas de números
-	boolean validaTelefone(String telefone){		
+	
+	boolean validaTelefone(String telefone){
 		if (telefone.matches("[0-9]+"))
 			return true;
 		return false;
 	}
 	
-	//inicia a agenda apagando qualquer contato que nela possua.
-	//também seta o limite da memória para adicionar contatos.	
 	public boolean resetAgenda(int maxContatos) {
+		// TODO Auto-generated method stub
 		this.maxContatos = maxContatos;
-		this.listaContatos = new ArrayList<Contato>();
+		
 		this.listaContatos.removeAll(listaContatos);
 		return true;
 	}
 
-	//Adiciona um contato a agenda passando nome e telefone
-	//Nao podem existir duas entradas com o mesmo telefone
-	//
-	//O telefone deve ser composto apenas de digitos.
-	//O nome não pode estar vazio
-	//
-	//Se o contato foi adicionado com sucesso retorne true
 	public boolean adicionarContato(String telefone, String nome) {
+		// TODO Auto-generated method stub
 		Contato iContato = new Contato(telefone, nome);
 		if (getContatoByTel(telefone) == null) {
 			if (validaTelefone(telefone) && nome != null){
@@ -43,9 +36,9 @@ public class Cadastro{
 		}
 		return false;
 	}
-	
-	//Função que retorna o nome do contato a partir do seu telefone
+
 	public Contato getContatoByTel(String telefone) {
+		// TODO Auto-generated method stub
 		for (Contato iContato : listaContatos) {
 			if (iContato.getTelefone().equals(telefone)) {
 				return iContato;
@@ -53,11 +46,9 @@ public class Cadastro{
 		}
 		return null;
 	}
-	
-	//Atualiza o contato com esse Telefone
-	//retorne false caso telefone não seja encontrado ou
-	//nao esteja de acordo com os requisitos para adicionar um novo contato
+
 	public boolean atualizarContato(String telefone, String nome) {
+		// TODO Auto-generated method stub
 		for (Contato iContato : listaContatos) {
 			if(iContato.getTelefone().equals(telefone) && nome != null){
 				iContato.setNome(nome);
@@ -65,9 +56,9 @@ public class Cadastro{
 		}
 		return false;
 	}
-	
-	//remove o contato desse telefone
+
 	public boolean removerContato(String telefone) {
+		// TODO Auto-generated method stub
 		for (Contato iContato : listaContatos) {
 			if(iContato.getTelefone().equals(telefone)){
 				listaContatos.remove(iContato);
@@ -77,22 +68,19 @@ public class Cadastro{
 		return false;
 	}
 
-	//devolve todos os telefones da lista
 	public List<String> getContatos() {
+		// TODO Auto-generated method stub
 		List<String> listaTelefones = new ArrayList<String>();
 		
 		for (Contato iContato : listaContatos) {
-			listaTelefones.add(iContato.getTelefone());
+			listaTelefones.add(iContato.getTelefone()+" - "+iContato.getNome());
 		}
 
 		return listaTelefones;
 	}
 
-	//Dado o nome de um contato retorne a lista de telefones
-	//que ele possui. Caso o contato não exista, retorne null.
-	//A lista deve ser ordenada pela ordem de inserção na agenda,
-	//ou seja, os primeiros inseridos devem aparecer primeiro na lista
-	public List<String> getTelefones(String contato) {	
+	public List<String> getTelefones(String contato) {
+		// TODO Auto-generated method stub
 		List<String> listaTelefones = new ArrayList<String>();
 		
 		for (Contato iContato : listaContatos) {
